73d2bc7ea7309521888e0ea8679072c00c9f9b7c
Tiago0512006 <s.avendanop2@uniandes.edu.co>
2025-08-25 11:59:36 -0500

    Cambios aaray y single est-1

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -42,3 +42,82 @@ def size(my_list):
     Retorna el tamaño de la lista.
     """
     return my_list["size"]
+
+def first_element(my_list):
+    """
+    Retorna el primer elemento de la lista.
+    Si la lista está vacía, retorna None.
+    """
+    if my_list["size"] == 0:
+        return None
+    return my_list["elementos"][0]
+
+def is_empty(my_list):
+    """Retorna True si la lista está vacía"""
+    return my_list["size"] == 0
+
+def size(my_list):
+    """Retorna el tamaño de la lista"""
+    return my_list["size"]
+
+def last_element(my_list):
+    """Retorna el último elemento"""
+    if is_empty(my_list):
+        return None
+    return my_list["elementos"][-1]
+
+def delete_element(my_list, pos):
+    """Elimina un elemento en la posición dada"""
+    if pos < 0 or pos >= my_list["size"]:
+        return None
+    elem = my_list["elementos"].pop(pos)
+    my_list["size"] -= 1
+    return elem
+
+def remove_first(my_list):
+    """Elimina y retorna el primer elemento"""
+    if is_empty(my_list):
+        return None
+    elem = my_list["elementos"].pop(0)
+    my_list["size"] -= 1
+    return elem
+
+def remove_last(my_list):
+    """Elimina y retorna el último elemento"""
+    if is_empty(my_list):
+        return None
+    elem = my_list["elementos"].pop()
+    my_list["size"] -= 1
+    return elem
+
+def insert_element(my_list, element, pos):
+    """Inserta un elemento en la posición dada"""
+    if pos < 0 or pos > my_list["size"]:
+        return None
+    my_list["elementos"].insert(pos, element)
+    my_list["size"] += 1
+    return element
+
+def change_info(my_list, pos, element):
+    """Cambia el valor de un elemento en la posición dada"""
+    if pos < 0 or pos >= my_list["size"]:
+        return None
+    my_list["elementos"][pos] = element
+    return element
+
+def exchange(my_list, pos1, pos2):
+    """Intercambia dos elementos de posición"""
+    if pos1 < 0 or pos1 >= my_list["size"] or pos2 < 0 or pos2 >= my_list["size"]:
+        return None
+    my_list["elementos"][pos1], my_list["elementos"][pos2] = my_list["elementos"][pos2], my_list["elementos"][pos1]
+    return True
+
+def sub_list(my_list, pos, num):
+    """Retorna una sublista desde pos con num elementos"""
+    if pos < 0 or pos >= my_list["size"]:
+        return None
+    sub = {"elementos": [], "size": 0}
+    fin = min(pos + num, my_list["size"])
+    sub["elementos"] = my_list["elementos"][pos:fin]
+    sub["size"] = len(sub["elementos"])
+    return sub
--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -24,3 +24,114 @@ def is_present(my_list, element, cmp_function):
     if not is_in_array:
         count = -1
     return count
+
+def add_first(my_list, element):
+    """
+    Agrega un elemento al inicio de la lista enlazada.
+    """
+    new_node = {"info": element, "next": None}
+    if my_list["size"] == 0:
+        my_list["first"] = new_node
+        my_list["last"] = new_node
+    else:
+        new_node["next"] = my_list["first"]
+        my_list["first"] = new_node
+    my_list["size"] += 1
+
+
+def add_last(my_list, element):
+    """
+    Agrega un elemento al final de la lista enlazada.
+    """
+    new_node = {"info": element, "next": None}
+    if my_list["size"] == 0:
+        my_list["first"] = new_node
+        my_list["last"] = new_node
+    else:
+        my_list["last"]["next"] = new_node
+        my_list["last"] = new_node
+    my_list["size"] += 1
+
+
+def size(my_list):
+    """
+    Retorna el número de elementos de la lista.
+    """
+    return my_list["size"]
+
+
+def first_element(my_list):
+    """
+    Retorna el primer elemento de la lista.
+    Si la lista está vacía, retorna None.
+    """
+    if my_list["size"] == 0:
+        return None
+    return my_list["first"]["info"]
+
+def is_empty(my_list):
+    """Retorna True si la lista está vacía"""
+    return my_list["size"] == 0
+
+def size(my_list):
+    """Retorna el tamaño de la lista"""
+    return my_list["size"]
+
+def last_element(my_list):
+    """Retorna el último elemento"""
+    if is_empty(my_list):
+        return None
+    return my_list["last"]["info"]
+
+def delete_element(my_list, pos):
+    """Elimina el elemento en la posición dada"""
+    if pos < 0 or pos >= my_list["size"]:
+        return None
+
+    if pos == 0:  # eliminar el primero
+        return remove_first(my_list)
+
+    prev = my_list["first"]
+    for _ in range(pos - 1):
+        prev = prev["next"]
+
+    elem = prev["next"]["info"]
+    prev["next"] = prev["next"]["next"]
+
+    if pos == my_list["size"] - 1:  # si fue el último
+        my_list["last"] = prev
+
+    my_list["size"] -= 1
+    return elem
+
+def remove_first(my_list):
+    """Elimina y retorna el primer elemento"""
+    if is_empty(my_list):
+        return None
+    elem = my_list["first"]["info"]
+    my_list["first"] = my_list["first"]["next"]
+    if my_list["size"] == 1:
+        my_list["last"] = None
+    my_list["size"] -= 1
+    return elem
+
+def remove_last(my_list):
+    """Elimina y retorna el último elemento"""
+    if is_empty(my_list):
+        return None
+
+    if my_list["size"] == 1:
+        elem = my_list["first"]["info"]
+        my_list["first"] = None
+        my_list["last"] = None
+        my_list["size"] = 0
+        return elem
+
+    prev = my_list["first"]
+    while prev["next"] != my_list["last"]:
+        prev = prev["next"]
+
+    elem = my_list["last"]["info"]
+    prev["next"] = None
+
+
